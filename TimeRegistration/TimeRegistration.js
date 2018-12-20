0a5514c1ffaa8e2d854a36afe3f9792a722da858
Elena <littleyoru2000@gmail.com>
2018-12-20 14:20:04 +0100

    UI work and get Tasks api call

--- TimeRegistration/TimeRegistration.js
+++ TimeRegistration/TimeRegistration.js
@@ -1,3 +1,14 @@
-$(document).ready(function(){
-    
+$(document).ready(function() {
+    $.get('http://localhost:54003/api/Tasks', function(data) {
+        console.log(data)
+
+        // $('#tasks').html($.map(data, function(){
+        //     return $('<option/>', {text: data.TaskName})
+        // }))
+
+        var select = $('#tasks')
+        $(data).each(function(){
+            select.append($('<option>').attr('value', this.TaskName).text(this.TaskName))
+        })
+    }, 'JSON')
 })
